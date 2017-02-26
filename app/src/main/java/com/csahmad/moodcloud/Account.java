30c3c09ae0e881c9f2604b958cdadb203243703a
ahmad <ahmad@ualberta.ca>
2017-02-26 10:35:39 -0700

    Removed username setter from account (should not be able to change username)

--- app/src/main/java/com/csahmad/moodcloud/Account.java
+++ app/src/main/java/com/csahmad/moodcloud/Account.java
@@ -22,11 +22,6 @@ public class Account {
         return this.username;
     }
 
-    public void setUsername(String username) {
-
-        this.username = username;
-    }
-
     public String getPassword() {
 
         return this.password;
