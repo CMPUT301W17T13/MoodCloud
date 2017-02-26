83ca6220216b1b15e4ba06c764fafd68199c063e
ahmad <ahmad@ualberta.ca>
2017-02-26 14:52:07 -0700

    Added PostFilterTest

---
+++ app/src/androidTest/java/com/csahmad/moodcloud/PostFilterTest.java
@@ -0,0 +1,17 @@
+package com.csahmad.moodcloud;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+/** Test the PostFilter class. */
+public class PostFilterTest extends ActivityInstrumentationTestCase2 {
+
+    public PostFilterTest() {
+
+        super(MainActivity.class);
+    }
+
+    public void testGetFilteredPosts() {
+
+        ;
+    }
+}
