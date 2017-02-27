e4304a5390bbe5a8af5249d15b3bc54e2e23bf7e
ahmad <ahmad@ualberta.ca>
2017-02-26 22:25:01 -0700

    Filled LocationTools tests

--- app/src/androidTest/java/com/csahmad/moodcloud/LocationToolsTest.java
+++ app/src/androidTest/java/com/csahmad/moodcloud/LocationToolsTest.java
@@ -20,7 +20,20 @@ public class LocationToolsTest extends ActivityInstrumentationTestCase2 {
         double distance = LocationTools.kmDistance(location1, location2);
         assertEquals(distance, 0.0);
 
-        ;
+        location1 = new double[]{0.0, 0.0, 7.0};
+        location2 = new double[]{0.0, 0.0, 55.0};
+        distance = LocationTools.kmDistance(location1, location2);
+        assertEquals(distance, 0.0);
+
+        location1 = new double[]{3.6, 2.8, 0.0};
+        location2 = new double[]{38.4, 29.8, 0.0};
+        distance = LocationTools.kmDistance(location1, location2);
+        assertEquals(distance, 4_736.0, 10.0);
+
+        location1 = new double[]{3.6, 2.8, 8.0};
+        location2 = new double[]{38.4, 29.8, 70.0};
+        distance = LocationTools.kmDistance(location1, location2);
+        assertEquals(distance, 4_736.0, 10.0);
     }
 
     public void testMDistance() {
@@ -30,6 +43,19 @@ public class LocationToolsTest extends ActivityInstrumentationTestCase2 {
         double distance = LocationTools.mDistance(location1, location2);
         assertEquals(distance, 0.0);
 
-        ;
+        location1 = new double[]{0.0, 0.0, 0.0};
+        location2 = new double[]{0.0, 0.0, 0.0};
+        distance = LocationTools.mDistance(location1, location2);
+        assertEquals(distance, 0.0);
+
+        location1 = new double[]{3.6, 2.8, 0.0};
+        location2 = new double[]{38.4, 29.8, 0.0};
+        distance = LocationTools.mDistance(location1, location2);
+        assertEquals(distance, 4_736_000.0, 10_000.0);
+
+        location1 = new double[]{3.6, 2.8, 8.0};
+        location2 = new double[]{38.4, 29.8, 70.0};
+        distance = LocationTools.mDistance(location1, location2);
+        assertEquals(distance, 4_736_000.0, 10_000.0);
     }
 }
