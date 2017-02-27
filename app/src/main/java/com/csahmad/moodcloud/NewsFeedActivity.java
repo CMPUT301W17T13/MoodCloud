53e87fd3568b0b6b4bcfe5b02dbd281ce1f6607a
ahmad <ahmad@ualberta.ca>
2017-02-27 11:14:50 -0700

    Changed MoodHistoryActivity to NewsFeedActivity

--- app/src/main/java/com/csahmad/moodcloud/NewsFeedActivity.java
+++ app/src/main/java/com/csahmad/moodcloud/NewsFeedActivity.java
@@ -3,13 +3,13 @@ package com.csahmad.moodcloud;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-/** The activity for viewing the signed in user's mood history. */
-public class MoodHistoryActivity extends AppCompatActivity {
+/** The activity for viewing the latest mood events from people the signed in user follows. */
+public class NewsFeedActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mood_history);
+        setContentView(R.layout.activity_news_feed);
     }
 }
