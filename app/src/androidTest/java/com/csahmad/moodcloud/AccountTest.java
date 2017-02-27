a1b35722c8d4a8fd76d8a50fa88418b21a4827a2
ahmad <ahmad@ualberta.ca>
2017-02-27 08:57:22 -0700

    Expanded tests

--- app/src/androidTest/java/com/csahmad/moodcloud/AccountTest.java
+++ app/src/androidTest/java/com/csahmad/moodcloud/AccountTest.java
@@ -14,28 +14,44 @@ public class AccountTest extends ActivityInstrumentationTestCase2 {
     }
 
     public void testEquals() {
+
         Profile profile = new Profile("test");
+        Profile profile2 = new Profile("test 2");
+
         Account account1 = new Account("test", "password", profile);
-        Account account2 = new Account("test", "password", profile);
+        Account account2 = new Account("test", "other password", profile2);
         assertTrue(account1.equals(account2));
+
+        account1 = new Account("test", "password", profile);
+        account2 = new Account("test2", "password", profile);
+        assertFalse(account1.equals(account2));
     }
 
     public void testUsername() {
+
         Profile profile = new Profile("test");
         Account account = new Account("test", "password", profile);
         assertEquals("test", account.getUsername());
     }
 
     public void testPassword() {
+
         Profile profile = new Profile("test");
+
         Account account = new Account("test", "password", profile);
+        assertEquals("password", account.getPassword());
+
         account.setPassword("test2");
         assertEquals("test2", account.getPassword());
     }
 
     public void testProfile() {
+
         Profile profile = new Profile("test");
+
         Account account = new Account("test", "password", profile);
+        assertEquals(profile, account.getProfile());
+
         Profile newprofile = new Profile("test2");
         account.setProfile(newprofile);
         assertEquals(newprofile, account.getProfile());
