0c1f047c93ee5bb8ed19101d665784dabfa4e5cf
ahmad <ahmad@ualberta.ca>
2017-02-26 20:48:14 -0700

    Updated LocationTools

--- app/src/main/java/com/csahmad/moodcloud/LocationTools.java
+++ app/src/main/java/com/csahmad/moodcloud/LocationTools.java
@@ -1,5 +1,10 @@
 package com.csahmad.moodcloud;
 
+import android.location.Location;
+
+// Fantastical distance calculator:
+// http://www.nhc.noaa.gov/gccalc.shtml
+
 /** Tools for locations in double[]{latitude, longitude, altitude} form. */
 public class LocationTools {
 
@@ -16,21 +21,26 @@ public class LocationTools {
      */
     public static double kmDistance(double[] location1, double[] location2) {
 
-        return 0;
+        return LocationTools.mDistance(location1, location2) / 1_000.0;
     }
 
     /**
-     * Returns the distance between the given locations.
+     * Returns the distance between the given locations in meters.
      *
      * <p>
      * Ignores elevation.
      *
      * @param location1 a location in the form {latitude, longitude, altitude}
      * @param location2 a location in the form {latitude, longitude, altitude}
-     * @return the distance between the given locations
+     * @return the distance between the given locations in meters
      */
-    public static double distance(double[] location1, double[] location2) {
+    public static double mDistance(double[] location1, double[] location2) {
 
-        return 0;
+        float[] results = new float[3];
+
+        Location.distanceBetween(location1[0], location1[1], location2[0], location2[1],
+                results);
+
+        return results[0];
     }
 }
