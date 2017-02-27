e5ed346e8d55d85522acf060535ce258849efd97
ahmad <ahmad@ualberta.ca>
2017-02-27 11:16:49 -0700

    Updated docstring for ProfileActivity

--- app/src/main/java/com/csahmad/moodcloud/ViewProfileActivity.java
+++ app/src/main/java/com/csahmad/moodcloud/ViewProfileActivity.java
@@ -3,7 +3,7 @@ package com.csahmad.moodcloud;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-/** The activity for viewing a {@link Profile}. */
+/** The activity for viewing the {@link Profile} of a user and the mood history of that user. */
 public class ViewProfileActivity extends AppCompatActivity {
 
     @Override
