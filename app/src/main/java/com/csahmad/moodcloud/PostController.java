5585874def14be3d590ff30c689c9a35178702f5
ahmad <ahmad@ualberta.ca>
2017-03-01 11:46:28 -0700

    Started adding controller classes

---
+++ app/src/main/java/com/csahmad/moodcloud/PostController.java
@@ -0,0 +1,37 @@
+package com.csahmad.moodcloud;
+
+import java.util.ArrayList;
+
+/**
+ * Created by oahmad on 2017-03-01.
+ */
+
+public class PostController {
+
+    // Note: stores post in LocoalData.homeProfile AND sends the post to the internets
+    public void addPost(Post post) {
+
+        ;
+    }
+
+    // Note: Finds post in elasticsearch with newPost.id and updates with new fields
+    public void updatePost(Post editedPost) {
+
+        ;
+    }
+
+    public Post getPost(String id) {
+
+        ;
+    }
+
+    public ArrayList<Post> getPosts(PostFilter filter) {
+
+        ;
+    }
+
+    public void deletePost(String postId) {
+
+        ;
+    }
+}
---
+++ app/src/main/java/com/csahmad/moodcloud/ProfileController.java
@@ -0,0 +1,25 @@
+package com.csahmad.moodcloud;
+
+import java.util.ArrayList;
+
+/**
+ * Created by oahmad on 2017-03-01.
+ */
+
+public class ProfileController {
+
+    public Profile getProfileFromUsername(String username) {
+
+        ;
+    }
+
+    public Profile getProfileFromID(String id) {
+
+        ;
+    }
+
+    public ArrayList<Profile> getProfiles(ProfileFilter filter) {
+
+        ;
+    }
+}
