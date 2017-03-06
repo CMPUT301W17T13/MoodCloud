026c9baa011762514f4396ee8d9c6ce338753cec
ahmad <ahmad@ualberta.ca>
2017-03-06 09:44:45 -0700

    Added nested class to add objects using elasticsearch and added ElasticSearchObject interface

--- app/src/main/java/com/csahmad/moodcloud/ElasticSearchController.java
+++ app/src/main/java/com/csahmad/moodcloud/ElasticSearchController.java
@@ -1,8 +1,12 @@
 package com.csahmad.moodcloud;
 
+import android.os.AsyncTask;
+import android.util.Log;
 import com.searchly.jestdroid.DroidClientConfig;
 import com.searchly.jestdroid.JestClientFactory;
 import com.searchly.jestdroid.JestDroidClient;
+import io.searchbox.core.DocumentResult;
+import io.searchbox.core.Index;
 
 /**
  * Created by oahmad on 2017-03-06.
@@ -15,6 +19,51 @@ public class ElasticSearchController {
 
     /** For building and executing save commands and search queries. */
     private static JestDroidClient client;
+
+    // AsyncTask<Params, Progress, Result>
+    /**
+     * For saving an object using elasticsearch.
+     */
+    public static class AddObject<T extends ElasticSearchObject> extends AsyncTask<T, Void, Void> {
+
+        /**
+         * Saves the given object(s) using elasticsearch.
+         *
+         * @param items the objects to save
+         * @return null
+         */
+        @Override
+        protected Void doInBackground(T... items) {
+
+            ElasticSearchController.setClientIfNull();        // Set up client if it is null
+
+            // Save each object
+            for (T item: items) {
+
+                Index index = new Index.Builder(item)
+                        .index(ElasticSearchController.index)
+                        .type(item.getTypeName())
+                        .build();
+
+                try {
+
+                    DocumentResult result = ElasticSearchController.client.execute(index);
+
+                    if (result.isSucceeded())
+                        item.setId(result.getId());
+
+                    else
+                        Log.i("Error", "Elasticsearch was not able to add the object");
+                }
+
+                catch (Exception e) {
+                    Log.i("Error", "The application failed to build and send the objects.");
+                }
+            }
+
+            return null;
+        }
+    }
 
     ;
 
---
+++ app/src/main/java/com/csahmad/moodcloud/ElasticSearchObject.java
@@ -0,0 +1,12 @@
+package com.csahmad.moodcloud;
+
+/**
+ * Created by oahmad on 2017-03-06.
+ */
+
+public interface ElasticSearchObject {
+
+    public String getId();
+    public void setId(String id);
+    public String getTypeName();
+}
