8aea6225aba4f2ac66ac594c8a5e29b4cc67929a
ahmad <ahmad@ualberta.ca>
2017-03-06 09:25:08 -0700

    Added ElasticSearchController

---
+++ app/src/main/java/com/csahmad/moodcloud/ElasticSearchController.java
@@ -0,0 +1,13 @@
+package com.csahmad.moodcloud;
+
+/**
+ * Created by oahmad on 2017-03-06.
+ */
+
+public class ElasticSearchController {
+
+    private static String url = "http://cmput301.softwareprocess.es:8080";
+    private static String index = "CMPUT301W17T13";
+
+    ;
+}
