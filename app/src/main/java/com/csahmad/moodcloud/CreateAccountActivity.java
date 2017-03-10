8f12318e7da25532e1daeea440ae87ac7d23b4d0
taylorbos <bos@ualberta.ca>
2017-03-09 20:45:23 -0700

    started create account activity

--- app/src/main/java/com/csahmad/moodcloud/CreateAccountActivity.java
+++ app/src/main/java/com/csahmad/moodcloud/CreateAccountActivity.java
@@ -1,7 +1,13 @@
 package com.csahmad.moodcloud;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 /** The activity for creating an {@link Account}. */
 public class CreateAccountActivity extends AppCompatActivity {
@@ -11,5 +17,31 @@ public class CreateAccountActivity extends AppCompatActivity {
         
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ProfileController profileController = new ProfileController();
+        AccountController accountController = new AccountController();
+
+        final EditText usernameText = (EditText) findViewById(R.id.username);
+        final EditText passwordText = (EditText) findViewById(R.id.password);
+
+        Button button = (Button) findViewById(R.id.create);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (AccountController.isUsernameUnique()) {
+                    Profile profile = new Profile(usernameText.getText().toString());
+                    
+                    Account account = new Account(usernameText.getText().toString(), passwordText.getText().toString(), profile);
+                    accountController.addAccount(account);
+                    //probably something to sign in the user
+                    Context context = view.getContext();
+                    Intent intent = new Intent(context, NewsFeedActivity.class);
+                    startActivity(intent);
+
+                }
+
+            }
+        });
     }
 }
