1c287e22167cb653abdbb079030f0c96bec99d28
taylorbos <bos@ualberta.ca>
2017-03-11 18:09:49 -0700

    done newsfeed activity

--- app/src/main/java/com/csahmad/moodcloud/NewsFeedActivity.java
+++ app/src/main/java/com/csahmad/moodcloud/NewsFeedActivity.java
@@ -3,7 +3,6 @@ package com.csahmad.moodcloud;
 import android.content.Context;
 import android.content.Intent;
 import android.database.DataSetObserver;
-import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -11,10 +10,12 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.concurrent.TimeoutException;
 
 import static java.lang.Boolean.TRUE;
 
@@ -32,28 +33,59 @@ public class NewsFeedActivity extends AppCompatActivity {
         setContentView(R.layout.activity_news_feed);
         mRecyclerView = (RecyclerView) findViewById(R.id.postList);
 
-        mRecyclerView.setHasFixedSize(TRUE);
-
         mLayoutMananger = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutMananger);
 
-        mAdapter = new MyAdapter(postController.getPosts(myDataset));
-        mRecyclerView.setAdapter(mAdapter);
+        try{
+            ArrayList<Post> mDataset = postController.getFolloweePosts(LocalData.getSignedInProfile(),null,0);
+            mAdapter = new MyAdapter(mDataset);
+            mRecyclerView.setAdapter(mAdapter);
+        } catch (TimeoutException e){
+            System.err.println("TimeoutException: " + e.getMessage());
+        }
+
+        ImageButton imageButton = (ImageButton) findViewById(R.id.backButton);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                Context context = view.getContext();
+                Intent intent = new Intent(context, SignInActivity.class);
+                startActivity(intent);
+            }}
+        );
+
+        Button followingButton = (Button) findViewById(R.id.followingButton);
+        followingButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Context context = view.getContext();
+                Intent intent = new Intent(context, FollowingActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button profileButton = (Button) findViewById(R.id.profileButton);
+        profileButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Context context = view.getContext();
+                Intent intent = new Intent(context, ViewProfileActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
         private ArrayList<Post> mDataset;
 
         public class ViewHolder extends RecyclerView.ViewHolder {
-            public View mView;
             public TextView mNameView;
             public TextView mMoodView;
-            public Post mPost;
-            public Profile mProfile;
+            public TextView mTextView;
 
             public ViewHolder(View v) {
                 super(v);
-                mView = v;
+                mTextView = (TextView) v.findViewById(R.id.postText);
                 mNameView = (TextView) v.findViewById(R.id.postName);
                 mMoodView = (TextView) v.findViewById(R.id.postMood);
             }
@@ -64,9 +96,9 @@ public class NewsFeedActivity extends AppCompatActivity {
         }
 
         @Override
-        public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            TextView v = (TextView) LayoutInflater.from(parent.getContext())
-                    .inflate(R.layout.activity_news_feed, parent, false);
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.news_feed_item, parent, false);
 
             ViewHolder vh = new ViewHolder(v);
             return vh;
@@ -74,10 +106,11 @@ public class NewsFeedActivity extends AppCompatActivity {
 
         @Override
         public void onBindViewHolder(final ViewHolder holder, int position) {
-            holder.mPost = mDataset.get(position);
-            holder.mProfile = holder.mPost.getPoster();
-            holder.mNameView.setText(holder.mProfile.getName());
-            holder.mMoodView.setText(holder.mPost.getMood());
+            Post post = mDataset.get(position);
+            Profile profile = post.getPoster();
+            holder.mNameView.setText(profile.getName());
+            holder.mTextView.setText(post.getText());
+            holder.mMoodView.setText(post.getMood());
         }
 
         @Override
