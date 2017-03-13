3c7cbfc3aa917720fce8473ccd83ddc530ff8fbd
linghou <linghou@ualberta.ca>
2017-03-12 20:36:14 -0600

    started on the addpost activity a bit

--- app/src/main/java/com/csahmad/moodcloud/AddOrEditPostActivity.java
+++ app/src/main/java/com/csahmad/moodcloud/AddOrEditPostActivity.java
@@ -2,6 +2,8 @@ package com.csahmad.moodcloud;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.EditText;
 
 /** The activity for adding a {@link Post} or editing an existing one. */
 public class AddOrEditPostActivity extends AppCompatActivity {
@@ -11,5 +13,12 @@ public class AddOrEditPostActivity extends AppCompatActivity {
         
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_or_edit_post);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        final PostController postController = new PostController();
+
+        final EditText textExplanation = (EditText) findViewById(R.id.body);
+        final EditText testTrigger = (EditText) findViewById(R.id.trigger);
     }
 }
