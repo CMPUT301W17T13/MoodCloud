5a215d63ccd2fb690155a1cb81bc93ad30b800e4
Erick S <esieben@ualberta.ca>
2017-04-01 07:54:28 -0600

    Re-added UI testing changes
    Not breaking anything thistime

--- app/src/androidTest/java/com/csahmad/moodcloud/CreateAccountUITest.java
+++ app/src/androidTest/java/com/csahmad/moodcloud/CreateAccountUITest.java
@@ -5,9 +5,12 @@ import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.After;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.concurrent.TimeoutException;
 
 import static android.support.test.espresso.Espresso.closeSoftKeyboard;
 import static android.support.test.espresso.Espresso.onData;
@@ -32,38 +35,64 @@ import static org.hamcrest.Matchers.anything;
 
 @RunWith(AndroidJUnit4.class)
 public class CreateAccountUITest {
+    private ProfileController PC;
+    private AccountController AC;
 
-    //---|CREATE ACCOUNT ACTIVITY TESTS|---
 
 
     @Rule
-    public ActivityTestRule<CreateAccountActivity> createAccountRule = new ActivityTestRule<CreateAccountActivity>(CreateAccountActivity.class);
+    public ActivityTestRule<CreateAccountActivity> createAccountRule = new ActivityTestRule<CreateAccountActivity>(CreateAccountActivity.class){
+
+         /**
+          * Set up required objects for testing
+          */
+         @Override
+         protected void beforeActivityLaunched(){
+            PC = new ProfileController();
+            AC = new AccountController();
+         }
+
+         /**
+          * Clean up created test account/profile
+          */
+         @Override
+         protected void afterActivityFinished() {
+            try{
+                Account currentAccount = AC.getAccountFromUsername("JohnSmith");
+                if(currentAccount != null) {
+                    AC.deleteAccounts(currentAccount);
+                }
+            }
+          catch(TimeoutException e){}
+         }
+    };
 
     /**
-     * Tests that creating a non-unique account holds user at create account screen
-     *
-     * username = password = "esieben"
-    */
+     * Tests that creating an account signs the user in
+     */
 
-     @Test
-     public void createAccountTest() {
-     onView(withId(R.id.username)).perform(typeText("esieben"), ViewActions.closeSoftKeyboard());
-     onView(withId(R.id.password)).perform(typeText("esieben"), ViewActions.closeSoftKeyboard());
-     onView(withId(R.id.create)).perform(click());
+    @Test
+    public void createAccountTest() {
+         onView(withId(R.id.username)).perform(typeText("JohnSmith"), ViewActions.closeSoftKeyboard());
+         onView(withId(R.id.password)).perform(typeText("123456"), ViewActions.closeSoftKeyboard());
+         onView(withId(R.id.create)).perform(click());
 
-     onView(withId(R.id.title)).check(matches(withText("Create Account")));
-     }
+         //on news feed activity
+         onView(withId(R.id.profileButton)).perform(click());
 
-     /**
-      * Tests that the arrow button in the top left corner of the screen
-      * returns the user to the sign in screen
-      * */
+         //on view profile activity
+         onView(withId(R.id.profileName)).check(matches(withText("Name: JohnSmith")));
+    }
 
-     @Test
-     public void backTest(){
-     onView(withId(R.id.backButton)).perform(click());
+    /**
+     * Tests that the arrow button in the top left corner of the screen
+     * returns the user to the sign in screen
+     * */
 
-     onView(withId(R.id.title)).check(matches(withText("Sign In")));
-     }
+    @Test
+    public void backTest(){
+         onView(withId(R.id.backButton)).perform(click());
 
- }
+         onView(withId(R.id.title)).check(matches(withText("Sign In")));
+    }
+}
--- app/src/androidTest/java/com/csahmad/moodcloud/SignInUITest.java
+++ app/src/androidTest/java/com/csahmad/moodcloud/SignInUITest.java
@@ -5,6 +5,8 @@ import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -31,51 +33,65 @@ import static org.hamcrest.Matchers.anything;
  */
 
 @RunWith(AndroidJUnit4.class)
-public class SignInUITest {
+    public class SignInUITest {
+        Profile testProfile = new Profile("JohnSmith");
+        Account testAccount = new Account("JohnSmith", "123456", testProfile);
+        ProfileController PC = new ProfileController();
+        AccountController AC = new AccountController();
 
+        @Rule
+        //Rule used to instantiate activity on which the following tests will be performed
+        public ActivityTestRule<SignInActivity> signInRule = new ActivityTestRule<SignInActivity>(SignInActivity.class);
 
-    //---|SIGN IN ACTIVITY TESTS|---
-
-    @Rule
-    //Rule used to instantiate activity on which the following tests will be performed
-    public ActivityTestRule<SignInActivity> signInRule = new ActivityTestRule<SignInActivity>(SignInActivity.class);
-
-    /**
-     * Tests that signing in brings the user to the newsfeed page
-     *
-     * uses username = password = "esieben" (existing account)
-     */
-    @Test
-    public void loginTest() {
-        //target the desired interface element and on it, perform the necessary actions
-        //Enter a name into the username field
-        onView(withId(R.id.username)).perform(typeText("esieben"), ViewActions.closeSoftKeyboard());
-        //Enter a password into the password field
-        onView(withId(R.id.password)).perform(typeText("esieben"), ViewActions.closeSoftKeyboard());
-        try {
-            Thread.sleep(500);
+        /**
+         * Create {@Link Account} and {@Link Profile}that will be used in the tests
+         */
+        @Before
+        public void setUp(){
+            PC.addOrUpdateProfiles(testProfile);
+            AC.addOrUpdateAccounts(testAccount);
         }
-        catch(InterruptedException e){
+
+        /**
+         * Remove objects created for testing
+         */
+        @After
+        public void cleanUp(){
+            PC.deleteProfiles(testProfile);
+            AC.deleteAccounts(testAccount);
+        }
+
+        /**
+         * Tests that signing in causes the user's profile to be loaded
+         *
+         *
+         */
+        @Test
+        public void loginTest() {
+            //target the desired interface element and on it, perform the necessary actions
+            //Enter a name into the username field
+            onView(withId(R.id.username)).perform(typeText("JohnSmith"), ViewActions.closeSoftKeyboard());
+            //Enter a password into the password field
+            onView(withId(R.id.password)).perform(typeText("123456"), ViewActions.closeSoftKeyboard());
+            //Click the "sign in" button
+            onView(withId(R.id.signIn)).perform(click());
+
+            //now on news feed activity
+            onView(withId(R.id.profileButton)).perform(click());
+
+            //now on profile activity
+            onView(withId(R.id.profileName)).check(matches(withText("Name: JohnSmith")));
 
         }
-        //Click the "sign in" button
-        onView(withId(R.id.signIn)).perform(click());
 
-        //now on news feed activity
-        //ensure that the toolbar title is "News Feed" (check that the screen changed to the
-        //correct activity)
-        onView(withId(R.id.title)).check(matches(withText("News Feed")));
+        /**
+         * Tests that using the "Create Account" button brings the user to the create account screen
+         */
+        @Test
+        public void goToCreateTest() {
+            onView(withId(R.id.createAccount)).perform(click());
 
-    }
-
-    /**
-     * Tests that using the "Create Account" button brings the user to the create account screen
-     */
-    @Test
-    public void goToCreateTest() {
-        onView(withId(R.id.createAccount)).perform(click());
-
-        onView(withId(R.id.title)).check(matches(withText("Create Account")));
-    }
+            onView(withId(R.id.title)).check(matches(withText("Create Account")));
+        }
 
 }
