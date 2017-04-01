b689bb2bd36a881ea06afb75c958d7f587aaec56
ahmad <ahmad@ualberta.ca>
2017-04-01 17:38:49 -0600

    Started filling out EditProfileActivity

--- app/src/main/java/com/csahmad/moodcloud/EditProfileActivity.java
+++ app/src/main/java/com/csahmad/moodcloud/EditProfileActivity.java
@@ -2,14 +2,32 @@ package com.csahmad.moodcloud;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 /** The activity for editing a {@link Profile}. */
 public class EditProfileActivity extends AppCompatActivity {
+
+    private Profile profile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+
+        this.profile = LocalData.getSignedInProfile(this);
+
+        EditText name = (EditText) this.findViewById(R.id.profileName);
+        name.setText(profile.getName());
+    }
+
+    /** Save the new info to elasticsearch and locally then exit this Activity. */
+    public void onSaveProfileClick(View v) {
+
+        ProfileController controller = new ProfileController();
+        controller.addOrUpdateProfiles(profile);
+        LocalData.store(profile, this);
+        this.finish();
     }
 }
