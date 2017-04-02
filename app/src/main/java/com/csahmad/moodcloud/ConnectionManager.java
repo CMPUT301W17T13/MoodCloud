b00f5a74bb98e01b7effc2a0d027506e6d1fc3fc
ahmad <ahmad@ualberta.ca>
2017-04-02 11:06:55 -0600

    Method to display 'Connection error' message

--- app/src/main/java/com/csahmad/moodcloud/ConnectionManager.java
+++ app/src/main/java/com/csahmad/moodcloud/ConnectionManager.java
@@ -3,6 +3,7 @@ package com.csahmad.moodcloud;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.widget.Toast;
 
 /** Check if connected to internet. */
 public class ConnectionManager {
@@ -14,5 +15,11 @@ public class ConnectionManager {
                 = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
         return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+    /** Display a message to the user indicating that there was some kind of connection error. */
+    public static void showConnectionError(Context context) {
+
+        Toast.makeText(context, "Connection error", Toast.LENGTH_LONG).show();
     }
 }
