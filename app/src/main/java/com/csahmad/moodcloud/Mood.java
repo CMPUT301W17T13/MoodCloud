37604baa4c7ab3a28001fd74f1490514ea60765f
ahmad <ahmad@ualberta.ca>
2017-04-05 14:34:07 -0600

    (For fun) map marker colours + details

--- app/src/main/java/com/csahmad/moodcloud/Mood.java
+++ app/src/main/java/com/csahmad/moodcloud/Mood.java
@@ -1,5 +1,7 @@
 package com.csahmad.moodcloud;
 
+import android.content.Context;
+
 /**
  * Represents a mood.
  *
@@ -15,6 +17,38 @@ public class Mood {
     public static final int SAD = 5;
     public static final int ASHAMED = 6;
     public static final int SURPRISED = 7;
+
+    public static String toString(int mood) {
+
+        switch (mood) {
+
+            case Mood.ANGRY:
+                return "Angry";
+
+            case Mood.CONFUSED:
+                return "Confused";
+
+            case Mood.DISGUSTED:
+                return "Disgusted";
+
+            case Mood.SCARED:
+                return "Scared";
+
+            case Mood.HAPPY:
+                return "Happy";
+
+            case Mood.SAD:
+                return "Sad";
+
+            case Mood.ASHAMED:
+                return "Ashamed";
+
+            case Mood.SURPRISED:
+                return "Surprised";
+        }
+
+        return null;
+    }
 
     public static int fromString(String moodString) {
 
@@ -48,6 +82,53 @@ public class Mood {
 
             default:
                 throw new IllegalArgumentException("Not a valid mood string.");
+        }
+    }
+
+    /**
+     * Return the colour associated with the given mood (in hex format with the leading "#").
+     *
+     * @param mood the mood to get the associated colour of
+     * @return Return the colour associated with the given mood (in hex format with the leading "#")
+     */
+    public static String toHexColor(int mood, Context context) {
+
+        switch (mood) {
+
+            case Mood.ANGRY:
+                //noinspection ResourceType
+                return context.getString(R.color.angryColor);
+
+            case Mood.CONFUSED:
+                //noinspection ResourceType
+                return context.getString(R.color.confusedColor);
+
+            case Mood.DISGUSTED:
+                //noinspection ResourceType
+                return context.getString(R.color.disgustedColor);
+
+            case Mood.SCARED:
+                //noinspection ResourceType
+                return context.getString(R.color.scaredColor);
+
+            case Mood.HAPPY:
+                //noinspection ResourceType
+                return context.getString(R.color.happyColor);
+
+            case Mood.SAD:
+                //noinspection ResourceType
+                return context.getString(R.color.sadColor);
+
+            case Mood.ASHAMED:
+                //noinspection ResourceType
+                return context.getString(R.color.ashamedColor);
+
+            case Mood.SURPRISED:
+                //noinspection ResourceType
+                return context.getString(R.color.surprisedColor);
+
+            default:
+                throw new IllegalArgumentException("Not a valid mood.");
         }
     }
 }
--- app/src/main/java/com/csahmad/moodcloud/ShowMapActivity.java
+++ app/src/main/java/com/csahmad/moodcloud/ShowMapActivity.java
@@ -1,6 +1,9 @@
 package com.csahmad.moodcloud;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 
@@ -8,6 +11,8 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -88,10 +93,47 @@ implements OnMapReadyCallback{
         mapFragment.getMapAsync(this);
     }
 
+    // Modified from:
+    // https://stackoverflow.com/questions/19076124/android-map-marker-color/33036461#33036461
+    // (StackOverflow user S.Thiongane, edited by user1732111)
+    // Accessed April 5, 2017
+    /**
+     * Convert the given colour from hex format to HSV format and return the hue.
+     *
+     * @param hexColor the colour to convert
+     * @return the hue of the given colour
+     */
+    private static float getHue(String hexColor) {
+
+        float[] hsv = new float[3];
+        Color.colorToHSV(Color.parseColor(hexColor), hsv);
+        return hsv[0];
+    }
+
+    // Modified from:
+    // https://stackoverflow.com/questions/19076124/android-map-marker-color/33036461#33036461
+    // (StackOverflow user S.Thiongane, edited by user1732111)
+    // Accessed April 5, 2017
+    /**
+     * Return the default map marker icon in the given colour.
+     *
+     * @param hexColor the colour of the returned marker icon
+     * @return the default map marker icon in the given colour
+     */
+    private static BitmapDescriptor getMarkerIcon(String hexColor) {
+
+        return BitmapDescriptorFactory.defaultMarker(ShowMapActivity.getHue(hexColor));
+    }
+
     public void onMapReady(GoogleMap googleMap) {
 
         LatLng latLng = null;
         double[] location;
+        ProfileController profileController = new ProfileController();
+        Profile poster = null;
+        MarkerOptions marker;
+        BitmapDescriptor markerIcon;
+        String snippet;
 
         for (Post post: this.posts) {
 
@@ -100,8 +142,29 @@ implements OnMapReadyCallback{
             if (post.getLocation() != null) {
 
                 latLng = new LatLng(location[0], location[1]);
-                googleMap.addMarker(new MarkerOptions().position(latLng)
-                    .title(post.getText()));
+
+                markerIcon = ShowMapActivity.getMarkerIcon(
+                        Mood.toHexColor(post.getMood(), this));
+
+                marker = new MarkerOptions()
+                        .position(latLng)
+                        .title(post.getText())
+                        .icon(markerIcon);
+
+                try {
+                    poster = profileController.getProfileFromID(post.getPosterId());
+                }
+
+                catch (Exception e) {}
+
+                snippet = Mood.toString(post.getMood());
+
+                if (poster != null)
+                    snippet += " " + poster.getName();
+
+                marker.snippet(snippet);
+
+                googleMap.addMarker(marker);
             }
         }
 
